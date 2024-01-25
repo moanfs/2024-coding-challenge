5d9361fdbe96c348359d4bea2f02690c724711f0
moan <moanfs1@gmail.com>
2024-01-25 22:45:22 +0700

    day 2/365

---
+++ 2-reverseSeq.js
@@ -0,0 +1,18 @@
+// day 2/365 coding challenge 2024
+// https://www.codewars.com/kata/5a00e05cc374cb34d100000d/solutions/javascript
+
+// const reverseSeq = n => {
+
+//     const result = [];
+//     for(let i = n; i>=1; i--){
+//         result.push(i)
+//     }
+
+//     return result;
+//   };
+
+// const reverseSeq = n =>  Array(n).fill().map((el, i)=>i+1).reverse();
+const reverseSeq = n =>  Array(n).fill().map((el, i)=>n-i);
+    // return [...Array(n)]
+
+  console.log(reverseSeq(5));
