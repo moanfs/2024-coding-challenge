4c7d535bf6b37aceb0dec3a3ebd2728a8f3ceed3
moan <moanfs1@gmail.com>
2024-01-26 11:49:47 +0700

    day 3/365

---
+++ 3-hero.js
@@ -0,0 +1,16 @@
+// day 2/365 coding challenge 2024
+// https://www.codewars.com/kata/59ca8246d751df55cc00014c/train/javascript
+// function hero(bullets, dragons){
+//     //Get Coding!
+//     if(bullets /2 >= dragons){
+//         return true
+//     }else{
+//         return false
+//     }
+
+    // }
+// function hero(bullets, dragons){return (bullets /2 >= dragons)    }
+
+const hero = (bullets, dragons) => bullets /2 >= dragons;
+    console.log(hero(10, 5));
+    
