a11e8d2d066d6a1ca1640e85e081fa5ac10b4ef3
moan <moanfs1@gmail.com>
2024-02-03 00:44:48 +0700

    day 4/365

---
+++ 4-smash.js
@@ -0,0 +1,20 @@
+// day 2/365 coding challenge 2024
+// https://www.codewars.com/kata/53dc23c68a0c93699800041d/train/javascript
+
+// function smash (words) {
+//     return words.join(' ');
+//  };
+
+// const smash = (words) => words.join(' ');
+
+function smash (words) {
+    let result = '';
+    for(let i = 0; i < words.length; i++){
+        result += words[i];
+        if(i != words.length -1)result += ' '
+    }
+    return result;
+ };
+
+
+ console.log(smash(["this", "is", "a", "really", "long", "sentence"]))
